bee82af9818535c3314a27366614845466783387
calmkeen <iyunseong159@gmail.com>
2022-08-12 19:03:24 +0900

    modern collection View를 보고 main View 구성중 다른 깃허브 참고로 build전 swiftUI를 사용한 preview 구성

--- ManageYourTime/View/MainView.swift
+++ ManageYourTime/View/MainView.swift
@@ -8,20 +8,27 @@
 import UIKit
 import SnapKit
 
-class MainView: UIViewController {
+class MainView: UIViewController, UICollectionViewDelegate {
     
+    enum Section {
+        case main
+    }
     var selectedIndex : Int?
-
     
-    var mainCollectionView: UICollectionView = {
+    var mainView = UIView()
+    var collectionView: UICollectionView = {
         let collectionView = UICollectionView()
         collectionView.backgroundColor = .gray
         return collectionView
     }()
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
     //cell같은 부분이나 데이터를 관리할때 사용하는 코드
-//    var dataSource :UICollectionViewDiffableDataSource<<#SectionIdentifierType: Hashable#>, <#ItemIdentifierType: Hashable#>>
+    //    var dataSource :UICollectionViewDiffableDataSource<<#SectionIdentifierType: Hashable#>, <#ItemIdentifierType: Hashable#>>
     override func viewDidLoad() {
         super.viewDidLoad()
+        ViewSizing()
+        makeView()
+        ConfigureNavItem()
         // Do any additional setup after loading the view.
     }
     //  * 앱 사용 중에 메모리가 부족할 때 불리는 메소드
@@ -30,13 +37,17 @@ class MainView: UIViewController {
         super.didReceiveMemoryWarning()
     }
     func makeView(){
-        self.view.addSubview(mainCollectionView)
+        self.view.addSubview(mainView)
+        mainView.addSubview(collectionView)
     }
     func configure(){
         
     }
     func ViewSizing(){
-        mainCollectionView.snp.makeConstraints{make in
+        mainView.snp.makeConstraints{ make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        collectionView.snp.makeConstraints{ make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
     }
@@ -49,13 +60,67 @@ extension MainView{
         navigationItem.largeTitleDisplayMode = .always
     }
     
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+    }
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let leadingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7),
+                                                   heightDimension: .fractionalHeight(1.0)))
+            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let trailingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.3)))
+            trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            let trailingGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3),
+                                                   heightDimension: .fractionalHeight(1.0)),
+                subitem: trailingItem, count: 2)
+            
+            let nestedGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.4)),
+                subitems: [leadingItem, trailingGroup])
+            let section = NSCollectionLayoutSection(group: nestedGroup)
+            return section
+            
+        }
+        return layout
+    }
+    
+    func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<MainView, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(indexPath.section), \(indexPath.item)"
+            cell.contentView.backgroundColor = .cornflowerBlue
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = 8
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+    }
 }
 
 
 import SwiftUI
 
 struct MainViewPreview: PreviewProvider{
-    static var previews: some View{
+    static var previews: some View {
         MainView().toPreview()
         TabBarController().toPreview()
     }
