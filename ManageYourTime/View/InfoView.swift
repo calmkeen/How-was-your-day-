e667c5daf92124bf88360ed73d571f99fc56fdbc
calmkeen <iyunseong159@gmail.com>
2022-08-19 17:33:39 +0900

    이거 modern collectionView 따온건데 언제 snpakit으로 바꾸나~~

--- ManageYourTime/View/InfoView.swift
+++ ManageYourTime/View/InfoView.swift
@@ -17,7 +17,7 @@ class InfoView: UIViewController{
         info.backgroundColor = .white
         return info
     }()
-    
+    //값들이 반환될때 줄당 몇개씩 표시할 것인지 ex) list는 한줄로 grid는 줄당 5개 grid3 는 줄당 3개
     enum SectionLayoutKind: Int, CaseIterable {
         case list, grid5, grid3
         var columnCount: Int {
@@ -39,13 +39,44 @@ class InfoView: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItemSet()
-        MakeView()
-        Sizing()
+        configureHierarchy()
+        configureDataSource()
+        
+        //MakeView()
+        //Sizing()
     }
 
 }
 
 extension InfoView {
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
+
+            // The group auto-calculates the actual item width to make
+            // the requested number of columns fit, so this widthDimension is ignored.
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                 heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            let groupHeight = columns == 1 ?
+                NSCollectionLayoutDimension.absolute(44) :
+                NSCollectionLayoutDimension.fractionalWidth(0.2)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
     
     func navigationItemSet(){
 //        navigationItem.title = "InfomationView"
@@ -53,12 +84,66 @@ extension InfoView {
         navigationItem.largeTitleDisplayMode = .always
     }
     func MakeView(){
-        view.addSubview(InfoView)
+        //view.addSubview(InfoView)
+        //view.addSubview(collectionView)
     }
     func Sizing(){
         InfoView.snp.makeConstraints{ make in
             make.edges.equalToSuperview()
         }
+    }
+}
+
+extension InfoView {
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    func configureDataSource() {
+        
+        let listCellRegistration = UICollectionView.CellRegistration<ListCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+        }
+        
+        let textCellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.backgroundColor = .lightGray
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = SectionLayoutKind(rawValue: indexPath.section)! == .grid5 ? 8 : 0
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return SectionLayoutKind(rawValue: indexPath.section)! == .list ?
+            collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: identifier) :
+            collectionView.dequeueConfiguredReusableCell(using: textCellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        let itemsPerSection = 10
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        SectionLayoutKind.allCases.forEach {
+            snapshot.appendSections([$0])
+            let itemOffset = $0.rawValue * itemsPerSection
+            let itemUpperbound = itemOffset + itemsPerSection
+            snapshot.appendItems(Array(itemOffset..<itemUpperbound))
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension InfoView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
 
